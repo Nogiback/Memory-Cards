57e42f66874810fb8d1093ef95a2f0399f390a1c
Nogiback <peter.toan.do@gmail.com>
2023-11-27 16:19:14 -0500

    Initial project setup with dependencies, files, etc.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Memory-Cards
-React - Memory Card Game Project - The Odin Project
+
+[React - Memory Card Game Project - The Odin Project](https://www.theodinproject.com/lessons/node-path-react-new-memory-card)
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Header from './components/Header.jsx';
+// import MainContent from './components/MainContent.jsx';
+// import Footer from './components/Footer.jsx';
+
+export default function App() {
+
+  return (
+    <>
+      {/* <Header/>
+      <MainContent/>
+      <Footer/> */}
+    </>
+  )
+}
