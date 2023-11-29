dae28fba8a825eae6ffffe0c95a77e10280688af
Nogiback <peter.toan.do@gmail.com>
2023-11-29 00:10:26 -0500

    Minor css/styling fixes for responsive design

--- src/components/Gameboard/Scoreboard.jsx
+++ src/components/Gameboard/Scoreboard.jsx
@@ -11,12 +11,12 @@ export default function Scoreboard({ totalScore, highScore }) {
     <div className='scoreboard'>
     {showRules ? (
       <div className='info-message'>
-        <p>Do not click the same Pokemon twice! There are 5 rounds total! Good luck!</p>
+        <p id='info'>Do not click the same Pokemon twice! There are 5 rounds total! Good luck!</p>
       </div>  
       ) : (
         <div className='scores'>
-          <p id='current-score'>Current Score: {totalScore}</p>
-          <p id='high-score'>High Score: {highScore}</p>
+          <p className='score'>Current Score: {totalScore}</p>
+          <p className='score'>High Score: {highScore}</p>
         </div>
     )}
       <button onClick={toggleInfo} type='button' id='info-btn'><i className='fa-solid fa-circle-info'></i></button>
